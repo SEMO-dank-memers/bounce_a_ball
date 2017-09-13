b5a33323defd2318fba29dccf92c066860c769d7
rocke97 <aarongunther97@gmail.com>
2017-09-13 16:35:38 -0500

    I messed up :(

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+	public void ChangesScene(string scenename){
+		Application.LoadLevel(scenename);
+	}
+}
