4b040d7a608812c7fddae4e5e5e69aa44fb33c99
Sladek <scsladek1s@semo.edu>
2017-09-13 10:36:26 -0500

    Changed background, music, sound on hit, etc.

---
+++ Assets/Scripts/BGTransition.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGTransition : MonoBehaviour {
+	// Use this for initialization
+	private float timeLeft = 10.0f;
+	private const float duration = 10.0f;
+	private float t = 0.0f;
+	private Color day = new Color();
+	private Color night = new Color();
+	private bool isDay = true;
+
+	void Start () {
+		setBackgroundColors();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if (timeLeft <= 0) {
+			if (isDay) {
+				if (t < 1.0) {
+					Camera.main.backgroundColor = Color.Lerp (day, night, t);
+					t += Time.deltaTime / duration;
+				}
+				else {
+					isDay = false;
+					t = 0.0f;
+					timeLeft = 30.0f;
+				}
+			} 
+			else {
+				if (t < 1.0) {
+					Camera.main.backgroundColor = Color.Lerp (night, day, t);
+					t += Time.deltaTime / duration;
+				}
+				else {
+					isDay = true;
+					t = 0.0f;
+					timeLeft = 30.0f;
+				}
+			}
+		}
+	}
+
+	void setBackgroundColors(){
+		day.r = 0.0f;
+		day.g = 1.0f;
+		day.b = 1.0f;
+		day.a = 0.0f;
+		night.r = 0.1f;
+		night.g = 0.1f;
+		night.b = 0.1f;
+		night.a = 0.0f;
+	}
+}
---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+	public AudioClip musicClip;
+	public AudioSource musicSource;
+	// Use this for initialization
+	void Start () {
+		musicSource.clip = musicClip;
+		musicSource.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)){
+			if (musicSource.isPlaying) {
+				musicSource.Pause ();
+			}
+			else {
+				musicSource.UnPause ();
+			}
+		}
+	}
+}
