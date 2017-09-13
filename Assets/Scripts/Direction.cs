cef714174a14c97d476fc4bb1cf77090a83bd6cd
Bryant <jtbryant1s@semo.edu>
2017-09-13 10:20:03 -0500

    first commit

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction : MonoBehaviour {
+	public float thrust;
+	public Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		rigidBody.AddForce (transform.up * thrust);
+		Quaternion rotation = transform.rotation;
+		transform.Rotate(Vector3.forward, Random.Range(0,360));
+	}
+
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = transform.rotation;
+        transform.Rotate(Vector3.forward, 5);
+    }
+
+	void OnCollisionEnter2D(){
+		rigidBody = GetComponent<Rigidbody2D>();
+		if (rigidBody.velocity.magnitude < 0.01) {
+			rigidBody.AddForce (transform.up * thrust);
+		}
+		Quaternion rotation = transform.rotation;
+		transform.Rotate(Vector3.forward, Random.Range(0,360));
+	}
+}
---
+++ Assets/Scripts/HitSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSound : MonoBehaviour {
+
+    public AudioSource MusicSource;
+    public AudioClip MusicClip;
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.name == "Ball(Clone)")
+        {
+            MusicSource.clip = MusicClip;
+            MusicSource.Play();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = Random.Range(0.8f, 1.5f);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
