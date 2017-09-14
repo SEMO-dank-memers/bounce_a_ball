458ceb65640585ecf892235b8e0daa82e6700d51
Michael Ranciglio <rancigliom@gmail.com>
2017-09-13 23:46:13 -0500

    Added sound to ball prefab for Floor collision
    The ball will now use the "Punch_02" sound effect when hitting the floor... fancy, I know.
    Also moved the audio files to the audio folder and the music files to the music folder.

---
+++ Assets/Scripts/HitSoundWall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSoundWall : MonoBehaviour { //I know it says wall, but it's only for the floor for now, it's technically a wall.
+
+	public AudioSource Source; // var to hold the audio's source; set within unity
+	public AudioClip Clip; // var to hold the audio clip; set within  unity
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		//called on Collision with a 2D rigidbody
+		if (c.gameObject.name == "Floor") //ensures that this sound only plays when the rocks hit the floor (Stephen wanted it like this, so blame him if it lowers our grade)
+		{
+			Source.clip = Clip; //sets the clip the source should play
+			Source.Play();
+		}
+	}
+}
