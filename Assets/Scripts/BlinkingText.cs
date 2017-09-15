427b1d3d3275d773d3e2d4b1aa7a138b6253bd88
rocke97 <aarongunther97@gmail.com>
2017-09-14 22:10:28 -0500

    Added some documentation to BlinkingText script

--- Assets/Scripts/BlinkingText.cs
+++ Assets/Scripts/BlinkingText.cs
@@ -8,8 +8,8 @@ public class BlinkingText : MonoBehaviour {
 	Text currentText;
 	// Use this for initialization
 	void Start () {
-		currentText = GetComponent<Text> ();
-		StartCoroutine (Blinking ());
+		currentText = GetComponent<Text> ();//attribute the button's text component with currentText
+		StartCoroutine (Blinking ());//start the Blinking coroutine within the first frame of the game
 	}
 
 	// Update is called once per frame
@@ -20,6 +20,7 @@ public class BlinkingText : MonoBehaviour {
 	public IEnumerator Blinking(){
 		bool isBlinking = true;
 		while (isBlinking){
+			//create blinking effect by alternating between empty text and "Press Start"
 			currentText.text = "";
 			yield return new WaitForSeconds (.5f);
 			currentText.text = "Press Start";
