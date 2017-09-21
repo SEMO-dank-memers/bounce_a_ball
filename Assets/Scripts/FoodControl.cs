2ad0878ddac1c7ab0760866bbe127b329a4d308d
rocke97 <aarongunther97@gmail.com>
2017-09-20 22:48:57 -0500

    Removed unneeded script, put game scene in Scenes folder, added food and food control to game scene

---
+++ Assets/Scripts/FoodControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Alternates between various food sprites whenever the AI eats the food
+
+public class FoodControl : MonoBehaviour {
+	public bool eaten = false; // set this to true in unity from any script and it will trigger the food controller
+	private bool running = false; // used to ensure Update dosent cause the respawner to run multiple times at once
+	public GameObject parent; //just used to get the children (in this case the food sprites) in order to alternate between them
+
+	void Update () {
+		if ((eaten == true) && (running == false)) {
+			running = true;
+			for(int i=0; i<7; i++){
+				parent.transform.GetChild(i).gameObject.SetActive(false); // set all the food sprites to inactive state
+			}
+			StartCoroutine (RespawnFood ());
+			}
+		}
+	IEnumerator RespawnFood()
+	{
+		int random = Random.Range(0, 6);
+		yield return new WaitForSecondsRealtime(5); // let the food be inactive for 5 seconds
+		parent.transform.GetChild (random).gameObject.SetActive (true); // randomly set one food sprite to active
+		running = false; // allow check to run again
+		eaten = false; // allow other program to cause the check to run again
+	}
+}
+	
